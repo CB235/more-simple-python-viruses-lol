1a4503967f8372234b08dc097ab224e49ec8b75a
CB235 <47418121+cb235@users.noreply.github.com>
2020-11-16 18:32:32 +0000

    Add files via upload
    yeee

---
+++ FREE FORTNITE 100% REAL - Copy.py
@@ -0,0 +1,13 @@
+import webbrowser
+webbrowser.open("https://www.youtube.com/watch?v=oHg5SJYRHA0", new = 1)
+webbrowser.open("https://pastebin.com/Zz0yk1Fy", new = 1)
+int1 = 0
+int(int1)
+amount = 50
+for i in range(amount):
+    
+    open(f"FREE VBUCKS FREE ROBUX 2020 100% REAL{int1}", "w")
+    int1 = int1 + 1
+
+
+###########################run this code at your own risk#################################
---
+++ vir2.py
@@ -0,0 +1,14 @@
+import webbrowser
+#webbrowser.open("https://www.youtube.com/watch?v=oHg5SJYRHA0", new = 1)
+#webbrowser.open("https://pastebin.com/Zz0yk1Fy", new = 1)
+int1 = 0
+int(int1)
+amount = 150
+for i in range(amount):
+    
+    open(f"FREE VBUCKS FREE ROBUX 2020 100% REAL{int1}", "w")
+    int1 = int1 + 1
+
+
+###########################run this code at your own risk#################################
+###########################for educational purposes only#########
